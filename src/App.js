5f0785661c2c050549906ea7b205cce12f138049
Velazquez <MVELAZQUEZ@ark.xstrata.int>
2022-02-07 23:16:46 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/auth/Login';
+import NuevaCuenta from './components/auth/NuevaCuenta';
+import Proyectos from './components/proyectos/Proyectos';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/nueva-cuenta" element={<NuevaCuenta/>}/>
+        <Route path="/proyectos" element={<Proyectos/>}/>
+      </Routes> 
+    </Router>
+  );
+}
+
+export default App;
